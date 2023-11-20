72f91932962e9d25a165ce15dd2d69cf98f2e3c3
urvi-creator <urvi.patel@strathmore.edu>
2023-11-20 21:02:13 +0300

    Second commit

--- Design.java
+++ Design.java
@@ -1,22 +1,27 @@
 import javax.swing.*;
 import javax.swing.JOptionPane;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+
 
 public class Design extends JFrame {
         public static void main(String[] args) {
+            JPanel panel= new JPanel();
             JFrame frame = new JFrame("ATTENDANCE MANAGEMENT SYSTEM");
-            String name1=JOptionPane.showInputDialog("Enter your Student ID");
-            String name2=JOptionPane.showInputDialog("Enter date");
-            JOptionPane.showMessageDialog(null,"Attendance recorded successfully");
-            JButton button1 = new JButton("Student ID");
-            frame.add(button1);
-            JButton button2 = new JButton("Password");
-            frame.add(button2);
             frame.setLayout(new FlowLayout());
-            frame.setSize(290, 200);
+            frame.setSize(500, 500);
             frame.setVisible(true);
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.add(panel);
+            panel.setLayout(null);
+            JLabel label= new JLabel("User");
+            label.setBounds(10, 20, 80, 25);
+            panel.add(label);
+
+
+
+
+
+
         }
 
                 }
