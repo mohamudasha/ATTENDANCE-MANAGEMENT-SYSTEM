c58adfcc1f9b977ac81697b92836315f0474e3b3
168010 <vicky.kanyama@strathmore.edu>
2023-11-21 22:28:12 +0300

    Attendance

--- Design.java
+++ Design.java
@@ -2,51 +2,69 @@ import javax.swing.*;
 import javax.swing.JOptionPane;
 import java.awt.*;
 
-
+// 'Design' class that extends JFrame, used to create a GUI for an attendance management system
 public class Design extends JFrame {
+    // Main method - the entry point of the program
         public static void main(String[] args) {
+            // Creating a JPanel to hold components
             JPanel panel= new JPanel();
+            // Creating a JFrame with a title
             JFrame frame = new JFrame("ATTENDANCE MANAGEMENT SYSTEM");
+            // Setting the size of the frame
             frame.setSize(500, 500);
+            // Setting the default close operation to exit the application
             frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            // Adding the panel to the frame
             frame.add(panel);
-
+            // Setting the layout of the panel to null for absolute positioning of components
             panel.setLayout(null);
 
+            // Creating a label for the username
             JLabel userlabel= new JLabel("User");
+            // Setting the position and size of the label
             userlabel.setBounds(10, 20, 80, 25);
+            // Adding the label to the panel
             panel.add(userlabel);
 
+            // Creating a text field for user input
             JTextField userText = new JTextField();
+            // Setting the position and size of the text field
             userText.setBounds(100,20,165,25);
+            // Adding the text field to the panel
             panel.add(userText);
 
+            // Creating a label for the user ID
             JLabel userIDLabel= new JLabel("User ID");
+            // Setting the position and size of the label
             userIDLabel.setBounds(10,50,80,25);
+            // Adding the label to the panel
             panel.add(userIDLabel);
 
+            // Creating a text field for user ID input
             JTextField userIDText = new JTextField();
+            // Setting the position and size of the text field
             userIDText.setBounds(100,50,165,25);
+            // Adding the text field to the panel
             panel.add(userIDText);
 
+            // Creating a label for the password
             JLabel passwordLabel= new JLabel("Password");
+            // Setting the position and size of the label
             passwordLabel.setBounds(10,80,80,25);
+            // Adding the label to the panel
             panel.add(passwordLabel);
 
+            // Creating a password field for password input
             JPasswordField passwordText=new JPasswordField();
+            // Setting the position and size of the password field
             passwordText.setBounds(100,80,165,25);
+            // Adding the password field to the panel
             panel.add(passwordText);
+
+            // Making the frame visible
             frame.setVisible(true) ;
 
-
-
-
-
-
-
-
         }
-
                 }
 
 
--- Hours.java
+++ Hours.java
@@ -1,9 +1,15 @@
+//class 'Hours' that represent hours related details and inherits 'Attendance' class variables
 public class Hours extends Attendance{
-
+    //Private instance variable for storing hours
     private int hours;
+    //Pivate instance variable for storing present hours
     private int presentHours;
+    //Private instance variable for storing absent hours
     private int absentHours;
+    //Private instance variable for storing status
     private String status;
+
+    //Constructor that initialize hours,present hours and absent hours
     public Hours(int hours,int presentHours,int absentHours,String status,int admissionNumber,String sName){
         super(admissionNumber,sName);
         this.hours=hours;
@@ -11,15 +17,19 @@ public class Hours extends Attendance{
         this.absentHours=absentHours;
         this.status=status;
     }
+    //Method to retrieve and return hours
     public int getHours(){
         return hours;
     }
+    //Method to retrieve and return present hours
     public int getPresentHours(){
         return presentHours;
     }
+    //Method to retrieve and return absent hours
     public int getAbsentHours(){
         return absentHours;
     }
+    //Method to retrieve and return status
     public String getStatus(){
         if(absentHours==10){
             return "Bad";
