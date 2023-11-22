7da5de4365ce9a5cbc720f86d9b403f74e4a044b
168010 <vicky.kanyama@strathmore.edu>
2023-11-22 22:38:21 +0300

    Attendance

--- Unit.java
+++ Unit.java
@@ -1,10 +1,13 @@
-public class Unit extends Attendance{
-    public Unit(String unitName,int admissionNumber,String sName){
-        super(admissionNumber,sName);
-    }
-    public void getUnitName(){
-    }
-    return unitName;
+public class Unit extends Attendance {
+    private String unitName;
+
+    public Unit(String unitName, int admissionNumber, String sName) {
+        super(admissionNumber, sName);
+        this.unitName = unitName;
     }
 
+    public String getUnitName() {
+        return unitName;
+    }
+}
 
